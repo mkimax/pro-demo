125cb2498ed5c93f72e9202024eecc83e9e3aac5
mk max <chapai2022@163.com>
2023-04-28 11:53:13 +0800

    登录页创建配置

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,13 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 const routes = (RouteRecordRaw = [
   {
     path: "/",
+    name: "home",
     component: () => import("../views/HomeView.vue"),
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("../views/Login.vue"),
   },
 ]);
 
